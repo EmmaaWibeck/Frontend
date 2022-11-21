60eb15bc76625ce4af50da35ac8314775b5c58dd
Emma Wibeck <Emmawibeck@gmail.com>
2022-11-21 12:30:00 +0100

    ändrat det sista

--- src/components/issue.jsx
+++ src/components/issue.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import '../App.css'
 
 const issue = ({ issue }) => {
+
   return (
     <div className='issue'>
-      <h2> {issue.subject} </h2>
-      <p> { issue.description } </p>
+      <div className='sub-status'>
+        <h2> {issue.subject} </h2>
+        <p className='g-color '> Status: { issue.status.status } </p>
+      </div>
+      <p className='pb border-b'> { issue.description } </p>
+      <small className='g-color mt '> Skapad av: { issue.customer.email } </small>
+      <div > 
+        <small className='g-color'> Skapat: { issue.created.slice(0,10) }</small> 
+        <small className='g-color'> Kl: { issue.created.slice(11, 16) }</small> 
+      </div>
     </div>
+
   )
 }
 
